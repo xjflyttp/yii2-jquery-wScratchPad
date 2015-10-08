e0cc20e75864d65835651a62e54e07254bc3a818
xjflyttp <xjflyttp@gmail.com>
2015-10-08 16:17:11 +0800

    init

---
+++ WscratchpadAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace xj\wscratchpad;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author xjflyttp <xjflyttp@gmail.com>
+ */
+class WscratchpadAsset extends AssetBundle
+{
+
+    public $sourcePath = '@bower/wScratchPad/src';
+    public $basePath = '@webroot/assets';
+    public $js = ['wScratchPad.js'];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+}
